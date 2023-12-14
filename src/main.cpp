8c1079df48be1dc234f668a46a1fc82c050a30fb
Blattvorhang <hanyi.cn@outlook.com>
2023-12-15 01:08:27 +0800

    updated timeTest

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@ static float original_result[DATANUM], speedup_result[DATANUM];
  * @brief Test the time consumed by a function.
  * @param data The array to be calculated.
  * @param len The length of the data.
- * @param sum_value The sum of the data.
- * @param max_value The max of the data.
  * @param result The result of the sorted data.
  * @param func The function to be tested.
  * @param test_num The number of times to test, for calculating the average time. (default: 5)
